c9b447f523639e5a5bc3828acecd12e109617b46
status-im-auto <auto@status.im>
2025-07-30 18:13:21 +0000

    Update documentation

--- assets/js/d83566ec.edc91f64.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunklogos_docs_template=self.webpackChunklogos_docs_template||[]).push([[1442],{85909:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"codex"}')}}]);
--- assets/js/f38492c2.939c5008.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunklogos_docs_template=self.webpackChunklogos_docs_template||[]).push([[2814],{92967:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"nomos"}')}}]);
